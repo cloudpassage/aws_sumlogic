e015c832b51d896de3ba99952657a355d1c35a58
Jye Lee <jlee@cloudpassage.com>
2017-09-05 09:15:56 -0700

    forgot () on functions

--- halo_metrics_to_sumologic.py
+++ halo_metrics_to_sumologic.py
@@ -13,13 +13,13 @@ class HaloMetrics():
     def run(self, event, context):
         try:
             with self.t:
-                self.metrics_utility.server_state_summary
-                self.metrics_utility.critical_issues_summary
-                self.metrics_utility.os_types_summary
-                self.metrics_utility.sw_packages_summary
-                self.metrics_utility.processes_summary
-                self.metrics_utility.local_accounts_summary
-                self.metrics_utility.sw_packages_summary
+                self.metrics_utility.server_state_summary()
+                self.metrics_utility.critical_issues_summary()
+                self.metrics_utility.os_types_summary()
+                self.metrics_utility.sw_packages_summary()
+                self.metrics_utility.processes_summary()
+                self.metrics_utility.local_accounts_summary()
+                self.metrics_utility.sw_packages_summary()
         except:
             return self.current_time
         return self.current_time
